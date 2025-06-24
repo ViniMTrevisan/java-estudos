3c794be5d42ce55e593765b3ed0c72414c0dd494
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-24 14:11:35 -0300

    adicionei validacao de resposta ao codigo da hipoteca

--- projetos-java/src/hipoteca/Main.java
+++ projetos-java/src/hipoteca/Main.java
@@ -7,18 +7,40 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int principal = 0;
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal (1K - 1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1000000){
+                break;
+            }
+            System.out.println("Insert a number between 1K and 1M");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        float annualInterest = 0;
+        float monthlyInterest = 0;
+        while (true) {
+            System.out.print("Annual Interest Rate (0 - 30): ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 0 && annualInterest <= 30){
+                monthlyInterest = (annualInterest / 100) / 12;
+                break;
+            }
+            System.out.println("Insert a number between 0 and 30");
+        }
 
-        System.out.print("Period (years): ");
-        int period = scanner.nextInt();
-        int periodMonths = period * 12;
-
-        float monthlyInterest = (annualInterest / 100) / 12;
+        int period = 0;
+        int periodMonths = 0;
+        while (true) {
+            System.out.print("Period (years - 1 - 30): ");
+            period = scanner.nextInt();
+            if (period >= 1 && period <= 30){
+                periodMonths = period * 12;
+                break;
+            }
+            System.out.println("Insert a number between 1 and 30");
+        }
 
         double x = (1 + monthlyInterest);
         float power = (float)Math.pow(x, periodMonths);
