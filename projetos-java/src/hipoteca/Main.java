e42c9d296e6b61db7cfa1181d9e48126b728589a
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-25 14:10:51 -0300

    troquei o codigo e organizei

--- projetos-java/src/hipoteca/Main.java
+++ projetos-java/src/hipoteca/Main.java
@@ -6,46 +6,48 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         int principal = 0;
-        while (true) {
-            System.out.print("Principal (1K - 1M): ");
-            principal = scanner.nextInt();
-            if (principal >= 1000 && principal <= 1000000){
-                break;
-            }
-            System.out.println("Insert a number between 1K and 1M");
-        }
-
-        float annualInterest = 0;
         float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
         while (true) {
-            System.out.print("Annual Interest Rate (0 - 30): ");
-            annualInterest = scanner.nextFloat();
-            if (annualInterest >= 0 && annualInterest <= 30){
-                monthlyInterest = (annualInterest / 100) / 12;
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
                 break;
-            }
-            System.out.println("Insert a number between 0 and 30");
+            System.out.println("Enter a value between 1000 and 1000000");
         }
 
-        int period = 0;
-        int periodMonths = 0;
         while (true) {
-            System.out.print("Period (years - 1 - 30): ");
-            period = scanner.nextInt();
-            if (period >= 1 && period <= 30){
-                periodMonths = period * 12;
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
                 break;
             }
-            System.out.println("Insert a number between 1 and 30");
+            System.out.println("Enter a value between 1 and 30");
         }
 
-        double x = (1 + monthlyInterest);
-        float power = (float)Math.pow(x, periodMonths);
-        float multiplication = monthlyInterest * power;
-        float mortgage = principal * (multiplication/(power - 1));
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
