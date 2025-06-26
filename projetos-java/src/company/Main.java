c9f70eb49c3037873526100f764a8d8530aced6f
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-26 16:28:23 -0300

    voltando ao exemplo do funcionario

--- projetos-java/src/company/Main.java
+++ projetos-java/src/company/Main.java
@@ -3,13 +3,11 @@ package company;
 public class Main {
 
     public static void main(String[] args) {
-        var browser = new Browser();
-        browser.
+        var employee1 = new Employee();
+        employee1.setBaseSalary(50_000);
+        employee1.setHourlyRate(20);
+        int wage1 = employee1.calculateWage(10);
+        System.out.println(wage1);
     }
 }
 
-// var employee1 = new Employee();
-//        employee1.setBaseSalary(50_000);
-//        employee1.setHourlyRate(20);
-//        int wage1 = employee1.calculateWage(10);
-//        System.out.println(wage1);
