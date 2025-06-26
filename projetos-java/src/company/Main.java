8bb525bbafa6b63e19c92646dd787fdeabb83d4d
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-26 16:27:13 -0300

    adicionei browser para aprender a reduzir o coupling e entender o porque de public e private em metodos

--- projetos-java/src/company/Main.java
+++ projetos-java/src/company/Main.java
@@ -2,11 +2,14 @@ package company;
 
 public class Main {
 
-    public static void main (String[] args){
-        var employee1 = new Employee();
-        employee1.setBaseSalary(50_000);
-        employee1.setHourlyRate(20);
-        int wage1 = employee1.calculateWage(10);
-        System.out.println(wage1);
+    public static void main(String[] args) {
+        var browser = new Browser();
+        browser.
     }
 }
+
+// var employee1 = new Employee();
+//        employee1.setBaseSalary(50_000);
+//        employee1.setHourlyRate(20);
+//        int wage1 = employee1.calculateWage(10);
+//        System.out.println(wage1);
