a7338512a8ba5a0e44a9461ff3d782a11f791c69
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-30 10:24:35 -0300

    tightly coupled code - antes de aprender interface

---
+++ projetos-java/src/taxes/TaxReport.java
@@ -0,0 +1,15 @@
+package taxes;
+
+public class TaxReport {
+    private TaxCalculator calculator;
+
+    public TaxReport(){
+        calculator = new TaxCalculator(100_000);
+    }
+
+    public void show(){
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
+}
+
