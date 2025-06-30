f1da89f950f86b634d8d770c7be787b3aa347c55
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-30 11:07:35 -0300

    normally use constructors, so that is what im leaving

--- projetos-java/src/taxes/Main.java
+++ projetos-java/src/taxes/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         var calculator = new TaxCalculator2018(100_000);
-        var report = new TaxReport();
-        report.show(calculator);
-        report.show(new TaxCalculator2019());
+        var report = new TaxReport(calculator);
+        report.show();
     }
 }
--- projetos-java/src/taxes/TaxReport.java
+++ projetos-java/src/taxes/TaxReport.java
@@ -3,17 +3,13 @@ package taxes;
 public class TaxReport {
     private TaxCalculator calculator;
 
-//    public TaxReport(TaxCalculator calculator){
-//        this.calculator = calculator;
-//    }
+    public TaxReport(TaxCalculator calculator){
+        this.calculator = calculator;
+    }
 
-    public void show(TaxCalculator calculator){
+    public void show(){
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
-
-//    public void setCalculator(TaxCalculator calculator) {
-//        this.calculator = calculator;
-//    }
 }
 
