f44e998253f5f62a7362be1fd9a2c0fea5436f3c
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-30 17:37:24 -0300

    troquei pra portugues

--- projetos-java/src/mytube/Main.java
+++ projetos-java/src/mytube/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         var video = new Video();
-        video.setFileName("birthday.mp4");
-        video.setTitle("Jennifer's birthday");
-        video.setUser(new User("john@domain.com"));
+        video.setFileName("aniversario.mp4");
+        video.setTitle("Aniversário do Vinicius");
+        video.setUser(new User("vinimtrevisan@gmail.com"));
 
         var encoder = new VideoEncoder();
         var database = new VideoDatabase();
